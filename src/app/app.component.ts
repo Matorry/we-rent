124ed59c5bab301af33e905d75228535b719b616
Chinfrito <Chinfrito@users.noreply.github.com>
2024-02-11 16:38:19 +0100

    quitados stashed changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { OurOfferListComponent } from './components/our-offer.list/our-offer.list.component';
-<<<<<<< Updated upstream
 import { OurWorkListComponent } from './components/our-work.list/our-work.list.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { OurMethodListComponent } from "./components/our-method.list/our-method.list.component";
 
 @Component({
   selector: 'app-root',
@@ -17,18 +17,8 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
     OurOfferListComponent,
     OurWorkListComponent,
     WelcomeComponent,
-  ],
-=======
-import { OurMethodListComponent } from "./components/our-method.list/our-method.list.component";
-
-
-@Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, OurOfferListComponent, OurWorkListComponent, OurMethodListComponent]
->>>>>>> Stashed changes
+    OurMethodListComponent
+  ]
 })
 export class AppComponent {
   title = 'we-rent';
