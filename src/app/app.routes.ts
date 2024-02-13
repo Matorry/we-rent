badc57f6db3ce1e8f41b56584c49d13b601ebafc
Matorry <rodri.martinceron@gmail.com>
2024-02-13 13:31:26 +0100

    Add home, about, contact, services pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((c) => c.AboutComponent),
+  },
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./pages/services/services.component').then(
+        (c) => c.ServicesComponent
+      ),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then(
+        (c) => c.ContactComponent
+      ),
+  },
+  { path: '**', redirectTo: 'home' },
+];
